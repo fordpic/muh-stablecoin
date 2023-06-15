cb229383c509b3bfd0de564e5bcc36a81774d1f1
Ford Pickert <fordpickert@gmail.com>
2023-06-15 18:55:13 -0500

    mint & burn functionality incorporated

--- src/MuhStablecoin.sol
+++ src/MuhStablecoin.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.18;
 
 import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /*
  * @title MuhStablecoin
@@ -13,6 +14,30 @@ import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensio
  * This is the ERC20 implementation of my stablecoin system. It is meant to be governed by MSCEngine
  */
 
-contract MuhStablecoin is ERC20Burnable {
+contract MuhStablecoin is ERC20Burnable, Ownable {
+    error MuhStablecoin__MustBeMoreThanZero();
+    error MuhStablecoin__BurnAmountExceedsBalance();
+    error MuhStablecoin__NotZeroAddress();
+
     constructor() ERC20("MuhStablecoin", "MSC") {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+
+        if (_amount <= 0) revert MuhStablecoin__MustBeMoreThanZero();
+        if (balance < _amount) revert MuhStablecoin__BurnAmountExceedsBalance();
+
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) revert MuhStablecoin__NotZeroAddress();
+        if (_amount <= 0) revert MuhStablecoin__MustBeMoreThanZero();
+
+        _mint(_to, _amount);
+        return true;
+    }
 }
