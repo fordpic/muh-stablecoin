655f704f46b3cd941b33f3d71961106dcbd0b60c
Ford Pickert <fordpickert@gmail.com>
2023-06-20 13:03:16 -0500

    deploy script complete, skele for unit test

--- script/DeployMSC.s.sol
+++ script/DeployMSC.s.sol
@@ -4,12 +4,37 @@ pragma solidity ^0.8.18;
 import {Script} from "forge-std/Script.sol";
 import {MuhStablecoin} from "../src/MuhStablecoin.sol";
 import {MSCEngine} from "../src/MSCEngine.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployMSC is Script {
+    address[] public tokenAddresses;
+    address[] public priceFeedAddresses;
+
     function run() external returns (MuhStablecoin, MSCEngine) {
-        vm.startBroadcast();
+        HelperConfig config = new HelperConfig();
+
+        (
+            address wethUsdPriceFeed,
+            address wbtcUsdPriceFeed,
+            address weth,
+            address wbtc,
+            uint256 deployerKey
+        ) = config.activeNetworkConfig();
+
+        tokenAddresses = [weth, wbtc];
+        priceFeedAddresses = [wethUsdPriceFeed, wbtcUsdPriceFeed];
+
+        vm.startBroadcast(deployerKey);
         MuhStablecoin msc = new MuhStablecoin();
-        // MSCEngine engine = new MSCEngine();
+        MSCEngine engine = new MSCEngine(
+            tokenAddresses,
+            priceFeedAddresses,
+            address(msc)
+        );
+
+        msc.transferOwnership(address(engine)); // have to transfer bc MSC is ownable
         vm.stopBroadcast();
+
+        return (msc, engine);
     }
 }
---
+++ test/unit/MSCEngineTest.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Test} from "forge-std/Test.sol";
+import {DeployMSC} from "../../script/DeployMSC.s.sol";
+import {MuhStablecoin} from "../../src/MuhStablecoin.sol";
+import {MSCEngine} from "../../src/MSCEngine.sol";
+
+contract MSCEngineTest is Test {
+    function setUp() public {}
+}
